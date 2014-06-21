b9097388e9fe164ecb9a05c1d4d7134bc7c8c029
swhite <swhite@g2llc.com>
2014-06-21 15:08:01 -0700

    Added test code, tweaked the project, added some documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,76 +1,85 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions implement the programming assignment:
+## (https://class.coursera.org/rprog-004/human_grading/view/courses/972139/assessments/3/submissions)
+## Please refer to that page for a full description of the assignment.
 
-## Write a short comment describing this function
-
+# Implement a simple cache. This is creating a closure, since the functions like set() and get()
+# operate on the environment variables ('x' and 'm') defined inside makeCacheMatrix()
+#
+# Args:
+#   x: The matrix to be cached
+#
+# Returns:
+#   A list of functions that can be called by name. It's similar to a virtual-method table in C++.
 makeCacheMatrix <- function(x = matrix()) {
-  # initalize cached value
-  m <- NULL
-  
-  # setter function, which also clears the cached result (if any)
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  
-  # getter function
-  get <- function() {
-    x
-  }
-  
-  # cache the calculated result
-  setInverse <- function(inverse) {
-    m <<- inverse
-  }
-  
-  
-  # retrieve the calculated result
-  getInverse <- function() {
-    m
-  }
-  
-  # return a handle to the function table (essentially)
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, debug = FALSE) {
-  m <- x$getInverse()
-  if(!is.null(m)) {
-    if (debug) {
-      message("returning cached data:")
-      print(m)
-      message()
+    # initalize cached value
+    m <- NULL
+    
+    # setter function, which also clears the cached result (if any)
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
     }
-    return(m)
-  }
-  data <- x$get()
-  if (debug) {
-    message("calculating inverse of:")
-    print(data)
-    message()
-  }
-  m <- solve(data)
-  if (debug) {
-    message("caching calculated inverse:")
-    print(m)
-    message()
-  }
-  x$setInverse(m)
-  m
+    
+    # getter function
+    get <- function() {
+        x
+    }
+    
+    # cache the calculated result
+    setInverse <- function(inverse) {
+        m <<- inverse
+    }
+    
+    
+    # retrieve the calculated result
+    getInverse <- function() {
+        m
+    }
+    
+    # return a handle to the function table (essentially)
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-c <- matrix(1:4, nrow=2, ncol=2)
-matrixMaker = makeCacheMatrix()
-matrixMaker$set(c)
 
-inv1 <- cacheSolve(matrixMaker, debug=TRUE)
-im1 = c %*% inv1
-im1
-inv2 <- cacheSolve(matrixMaker, debug=TRUE)
-im2 <- c %*% inv2
-im2
+# Calculate the inverse (if any) of a supplied matrix. This is a barebones implementation that lacks
+# several things:
+# 1) It should check that a square matrix is being passed in.
+# 2) It should have nicer error handling in the matrix is not invertable
+#    (see http://en.wikipedia.org/wiki/Invertible_matrix)
+# This uses a matrix cache as a data container and to allow the return of a cached value when possible.
+# In real life I might use the 'memoise' package
+#    (http://cran.r-project.org/web/packages/memoise/index.html)
+#
+# Args:
+#   x: a function list returned from a makeCacheMatrix() call.
+#   verbose: If TRUE, prints messages describing the internal activity. Default is FALSE
+#
+# Returns:
+#   The inverse matrix if it exists. If the matrix isn't invertable an error will be thrown.
+cacheSolve <- function(x, verbose = FALSE) {
+    m <- x$getInverse()
+    if (!is.null(m)) {
+        if (verbose) {
+            message("returning cached data:")
+            print(m)
+            message()
+        }
+        return(m)
+    }
+    data <- x$get()
+    if (verbose) {
+        message("calculating inverse of:")
+        print(data)
+        message()
+    }
+    m <- solve(data)
+    if (verbose) {
+        message("caching calculated inverse:")
+        print(m)
+        message()
+    }
+    x$setInverse(m)
+    m
+}
---
+++ testCacheMatrix.R
@@ -0,0 +1,40 @@
+source("cachematrix.R")
+
+c1 = matrix(1:4, nrow = 2, ncol=2)
+mm1 = makeCacheMatrix()
+mm1$set(c1)
+
+# first run calculates the inverse
+inv1 = cacheSolve(mm1, verbose=TRUE)
+im1 = c1 %*% inv1
+im1
+
+# second run uses the cache
+inv2 = cacheSolve(mm1, verbose=TRUE)
+im2 = c1 %*% inv2
+im2
+
+# ...and we got back the same matrix each time
+# this will return TRUE
+identical(inv1, inv2)
+
+# we can have multiple caches at the same time
+c2 = matrix(2:5, nrow = 2, ncol=2)
+mm2 = makeCacheMatrix()
+mm2$set(c2)
+
+# this will return FALSE
+identical(cacheSolve(mm1), cacheSolve(mm2))
+
+# and we can reuse the cache
+mm1$set(c2)
+# now this will return TRUE
+identical(cacheSolve(mm1), cacheSolve(mm2))
+
+# if the matrix can't be inverted...
+c3 = matrix(1:9, nrow = 3, ncol=3)
+mm3 = makeCacheMatrix()
+mm3$set(c3)
+# this throws an error and inv3 is not assigned to anything
+inv3 = cacheSolve(mm3)
+
